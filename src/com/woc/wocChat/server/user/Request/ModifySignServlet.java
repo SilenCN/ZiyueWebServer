12db004103b753fc8e45cf3a7c4657e6f5097d68
silen <1039795376@qq.com>
2016-10-10 22:30:26 +0800

    nothing

---
+++ src/com/woc/wocChat/server/user/Request/ModifySignServlet.java
@@ -0,0 +1,26 @@
+package com.woc.wocChat.server.user.Request;
+
+import com.woc.wocChat.server.user.Service.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * <pr>
+ *     link:http://localhost:8080/User/ModifySign?UserId=&Sign=
+ *
+ * </pr>
+ *
+ * Created by silen on 16-10-10.
+ */
+@WebServlet("/User/ModifySign")
+public class ModifySignServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().print(UserService.modifySign(Integer.parseInt(req.getParameterValues("UserId")[0]), req.getParameterValues("Sign")[0]));
+    }
+}
--- src/com/woc/wocChat/server/user/Service/UserService.java
+++ src/com/woc/wocChat/server/user/Service/UserService.java
@@ -9,8 +9,12 @@ import com.woc.wocChat.server.user.model.User;
  */
 public class UserService {
     private static final String ADD_USER_SQL = "insert into user(signUpTime,lastLogin) values(%d,%d)";
+    private static final String MODIFY_USER_SIGN_SQL ="update user set sign=\"%s\" where id=%d";
     public static boolean addUser(){
         return new SQLBase().executeSQL(String.format(ADD_USER_SQL,new Object[]{(int)System.currentTimeMillis(),(int)System.currentTimeMillis()}));
+    }
+    public static boolean modifySign(int userId,String sign){
+        return new SQLBase().executeSQL(String.format(MODIFY_USER_SIGN_SQL,new Object[]{sign,userId}));
     }
 
 }
