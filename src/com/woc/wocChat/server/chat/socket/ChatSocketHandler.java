6a3d3017a13946e259c7d303576ca63b8e05a136
silen <1039795376@qq.com>
2016-10-13 09:26:34 +0800

    nothing

--- src/com/woc/wocChat/server/chat/socket/ChatSocketHandler.java
+++ src/com/woc/wocChat/server/chat/socket/ChatSocketHandler.java
@@ -1,51 +1,45 @@
 package com.woc.wocChat.server.chat.socket;
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
 
 /**
  * Created by silen on 16-10-13.
  */
-public class ChatSocketHandler  implements Runnable {
+public class ChatSocketHandler implements Runnable {
 
-    private Socket socket;
+    private Socket client;
 
     public ChatSocketHandler(Socket client) {
-        socket = client;
+        this.client = client;
         new Thread(this).start();
     }
 
     public void run() {
         try {
-            // 读取客户端数据
-            DataInputStream input = new DataInputStream(socket.getInputStream());
-            String clientInputStr = input.readUTF();//这里要注意和客户端输出流的写方法对应,否则会抛 EOFException
-            // 处理客户端数据
-            System.out.println("客户端发过来的内容:" + clientInputStr);
-
-            // 向客户端回复信息
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-            System.out.print("请输入:\t");
-            // 发送键盘输入的一行
-            String s = new BufferedReader(new InputStreamReader(System.in)).readLine();
-            out.writeUTF(s);
-
-            out.close();
-            input.close();
-        } catch (Exception e) {
-            System.out.println("服务器 run 异常: " + e.getMessage());
-        } finally {
-            if (socket != null) {
-                try {
-                    socket.close();
-                } catch (Exception e) {
-                    socket = null;
-                    System.out.println("服务端 finally 异常:" + e.getMessage());
+            //获取Socket的输出流，用来向客户端发送数据
+            PrintStream out = new PrintStream(client.getOutputStream());
+            //获取Socket的输入流，用来接收从客户端发送过来的数据
+            BufferedReader buf = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            boolean flag = true;
+            while (flag) {
+                //接收从客户端发送过来的数据
+                String str = buf.readLine();
+                if (str == null || "".equals(str)) {
+                    flag = false;
+                } else {
+                    if ("bye".equals(str)) {
+                        flag = false;
+                    } else {
+                        //将接收到的字符串前面加上echo，发送到对应的客户端
+                        out.println("echo:" + str);
+                    }
                 }
             }
+            out.close();
+            client.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
